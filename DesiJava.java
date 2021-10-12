be56204a03b6ddcc8bd08f663b71c71ab44e7d2d
desiokta <desiokt10@gmail.com>
2021-10-12 17:11:58 +0700

    desijava

---
+++ DesiJava.java
@@ -0,0 +1,31 @@
+public class DesiJava {
+  public static void main(String[] args) {
+  	// Membuat nama alamat dari peserta
+    // first name , lastname, age, phone number, address, and other
+    //System.out.println("Hello World");
+    
+    // Inisialisasi
+    String first_name = "";
+    String last_name = "";
+    String phone_number;
+    String address = "";
+    int age = 0;
+    
+    // Input variabel
+    first_name = "Desi";
+    last_name = "Oktavia";
+    phone_number = "082287393166";
+    address = "Padang";
+    age = 21;
+    
+    
+    // Print in to screen
+    System.out.println("=======================================");
+    System.out.println("||      First name   = " + first_name );
+    System.out.println("||      Last name    = " + last_name);
+    System.out.println("||      Phone number = " + phone_number);
+    System.out.println("||      Address      = " + address);
+    System.out.println("||      Age          = " + age);
+    System.out.println("=======================================");
+  }
+}
